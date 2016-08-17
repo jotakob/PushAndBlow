1cfa14a156655bfd191bc55c27acd230c40fe721
Jerry <Jeremias.Boos@gmx.de>
2016-08-17 21:41:01 +0200

    simple door and switches with animation event trigger

---
+++ PushAndBlow/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour {
+
+	public Animator door_animator;
+	public bool initalstate;
+	void Start(){
+		door_animator.SetBool ("close", initalstate);
+	}
+
+	void switch_on(){
+		door_animator.SetBool ("close", false);
+	}
+
+	void switch_off(){
+		door_animator.SetBool ("close", true);
+	}
+}
---
+++ PushAndBlow/Assets/Scripts/Switch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public GameObject target; 
+	public bool invert = false;
+
+	void switch_on(){
+		if (invert) {
+			target.SendMessage ("switch_off", SendMessageOptions.DontRequireReceiver);
+
+		} else {
+			target.SendMessage ("switch_on", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	void switch_off(){
+		if (invert) {
+			target.SendMessage ("switch_on", SendMessageOptions.DontRequireReceiver);
+		} else {
+			target.SendMessage ("switch_off", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	
+}
