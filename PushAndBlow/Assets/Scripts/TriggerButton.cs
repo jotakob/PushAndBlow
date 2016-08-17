8bb5b6006d7d4819236341a66ab2fc3931184e58
Jerry <Jeremias.Boos@gmx.de>
2016-08-17 21:58:29 +0200

    added trigger for button

---
+++ PushAndBlow/Assets/Scripts/TriggerButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerButton : MonoBehaviour {
+
+	public Animator button_animator;
+
+	void OnTriggerEnter(Collider other){
+		button_animator.SetBool ("pressed", true);
+	}
+	void OnTriggerExit(Collider other){
+		button_animator.SetBool ("pressed", false);
+	}
+}
