3cc7f76bdbe9ec20e20b65603ba119d520bdb608
Jerry <Jeremias.Boos@gmx.de>
2016-08-18 15:55:55 +0200

    all the switch glory

---
+++ PushAndBlow/Assets/Scripts/SwitchCharTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCharTrigger : MonoBehaviour {
+
+	public Animator switch_anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.CompareTag ("Player")) {
+			if (Input.GetButtonDown ("Fire2")) {
+				switch_anim.SetBool ("on", !switch_anim.GetBool ("on"));
+			}
+		}
+	}
+}
