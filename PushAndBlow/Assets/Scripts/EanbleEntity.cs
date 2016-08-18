546b8008ea4371479efa6a1e603202eae6c9d869
supermutant36 <jonasblum@yahoo.de>
2016-08-18 22:03:16 +0200

    hebel complete

---
+++ PushAndBlow/Assets/Scripts/EanbleEntity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EanbleEntity : MonoBehaviour {
+	public GameObject target;
+	public int triggertimes = 1;
+
+	void switch_on(){
+		if(triggertimes <=0)
+			target.SetActive (true);
+		
+		triggertimes--;
+	}
+	void switch_off(){
+		//target.SetActive (false);
+	}
+}
